6de1ee6a09d365546e451be4ddf5f1bb75dfcaae
paul88vera <paul88vera@gmail.com>
2025-07-08 22:28:55 -0500

    landing page completed

--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -1,19 +1,78 @@
-import { Outlet, ScrollRestoration } from "react-router";
+import { useState } from "react";
+import { Outlet, ScrollRestoration, Link } from "react-router";
 
 export default function RootLayout() {
+  const [social, setSocial] = useState(false);
   return (
     <div className="flex row flex-nowrap w-full">
       <ScrollRestoration />
-      {/* <div className="sidebar w-[500px] h-[100svh] bg-slate-800 p-4"></div> */}
-      {/* <div className="main_container w-full h-[100svh] bg-slate-600"> */}
-      {/* <div className="topnav bg-slate-800 w-full h-[90px] p-2">
-        </div> */}
+
       <div className={`main-content w-full`}>
         <div className="main-content-bg"></div>
         <Outlet />
       </div>
-      {/* </div> */}
-      {/* <div></div> */}
+      <div
+        onMouseEnter={() => {
+          setSocial(true);
+        }}
+        onMouseLeave={() => {
+          setSocial(false);
+        }}
+        className="absolute bg-slate-950 p-2 z-20 bottom-0 right-10 md:right-20 text-white border-2 border-white flex flex-row gap-6 ease-in-out delay-300">
+        <div>© 2025 by Paul Vera</div>
+        {social & (window.innerWidth > 768) ? (
+          <div>
+            <Link
+              target="_blank"
+              to="https://www.linkedin.com/in/paul88vera/"
+              className="hover:text-amber-400">
+              LinkedIn{" "}
+            </Link>
+            |
+            <Link
+              target="_blank"
+              to="https://github.com/paul88vera/"
+              className="hover:text-amber-400">
+              {" "}
+              GitHub{" "}
+            </Link>
+            |
+            <Link
+              target="_blank"
+              to="https://www.instagram.com/myfarewell88/"
+              className="hover:text-amber-400">
+              {" "}
+              Instagram
+            </Link>
+          </div>
+        ) : null}
+        {window.innerWidth < 768 ? (
+          <div className="block md:hidden">
+            <Link
+              target="_blank"
+              to="https://www.linkedin.com/in/paul88vera/"
+              className="hover:text-amber-400">
+              LinkedIn{" "}
+            </Link>
+            |
+            <Link
+              target="_blank"
+              to="https://github.com/paul88vera/"
+              className="hover:text-amber-400">
+              {" "}
+              GitHub{" "}
+            </Link>
+            |
+            <Link
+              target="_blank"
+              to="https://www.instagram.com/myfarewell88/"
+              className="hover:text-amber-400">
+              {" "}
+              Instagram
+            </Link>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
